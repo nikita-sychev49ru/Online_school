d9e369196c053654016ecaf88d6ee80c135ba2db
nikita <hesoyame1998@gmail.com>
2025-11-12 02:34:34 +1100

    update CourseSerializer

--- studies/serializers.py
+++ studies/serializers.py
@@ -3,15 +3,23 @@ from rest_framework import serializers
 from studies.models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    """сериалайзер для курса"""
-    class Meta:
-        model = Course
-        fields = '__all__'
-
-
 class LessonSerializer(serializers.ModelSerializer):
-    """сериалайзер для урока"""
+    """сериализатор для урока"""
     class Meta:
         model = Lesson
         fields = '__all__'
+
+class CourseSerializer(serializers.ModelSerializer):
+    """сериализатор для курса"""
+    # получаем уроки, включенные в курс
+    lessons = LessonSerializer(many=True)
+
+    # получаем количество уроков в курсе
+    lessons_count = serializers.SerializerMethodField()
+
+    def get_lessons_count(self, obj):
+        return obj.lessons.count()
+
+    class Meta:
+        model = Course
+        fields = ['title', 'preview', 'description', 'lessons_count', 'lessons']
