aba3518d9d9135d6d0d1b2813fda736661b09902
nikita <hesoyame1998@gmail.com>
2025-11-12 02:51:37 +1100

    install django-filter, update views for users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, generics
+from rest_framework.filters import OrderingFilter
 
-# Create your views here.
+from users.models import User, Payment
+from users.serializers import UserSerializer, PaymentSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    """вьюсет представлений для объектов модели пользователя"""
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+class PaymentListAPIView(generics.ListAPIView):
+    """представление для списка платежей"""
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ['paid_course', 'paid_lesson', 'payment_method']
+    ordering_fields = ['payment_date',]
