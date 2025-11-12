85cf3918a38bbc97cec9cde2a3230e81930967d7
nikita <hesoyame1998@gmail.com>
2025-11-13 05:05:47 +1100

    add paginators.py and update serializers.py

--- studies/serializers.py
+++ studies/serializers.py
@@ -1,6 +1,8 @@
+from django.template.context_processors import request
 from rest_framework import serializers
 
-from studies.models import Course, Lesson
+from studies.models import Course, Lesson, Subscribe
+from studies.validators import VideoUrlValidator
 
 
 class LessonSerializer(serializers.ModelSerializer):
@@ -8,9 +10,18 @@ class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+        validators = [VideoUrlValidator(field=['title', 'description','video_url']),]
 
 class CourseSerializer(serializers.ModelSerializer):
     """сериализатор для курса"""
+    # получаем признак подписки пользователя на курс
+    is_subscribed = serializers.SerializerMethodField()
+
+    def get_is_subscribed(self, obj):
+        """Проверяем подписку текущего пользователя на курс"""
+        request = self.context.get('request')
+        return Subscribe.objects.filter(user=request.user, course=obj).exists()
+
     # получаем уроки, включенные в курс
     lessons = LessonSerializer(many=True, read_only=True)
 
@@ -22,4 +33,13 @@ class CourseSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Course
-        fields = ['title', 'preview', 'description', 'lessons_count', 'lessons']
+        fields = ['title', 'preview', 'description', 'is_subscribed', 'lessons_count', 'lessons']
+        validators = [VideoUrlValidator(field=['title', 'description']), ]
+
+
+class SubscribeSerializer(serializers.ModelSerializer):
+    """сериализатор для подписки на курс"""
+    class Meta:
+        model = Subscribe
+        fields = '__all__'
+        read_only_fields = ('user', 'created_at')
