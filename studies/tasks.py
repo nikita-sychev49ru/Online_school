dea09407e2f873a3c67ce828db5e390cdd0d73bd
nikita <hesoyame1998@gmail.com>
2025-11-16 03:25:03 +1100

    formated code

--- studies/tasks.py
+++ studies/tasks.py
@@ -1,12 +1,11 @@
 from celery import shared_task
+from django.core.mail import send_mail
+from studies.models import Subscribe
+from config import settings
 
 @shared_task
 def send_course_update_email(course_id):
     """отправка письма об обновлении курса для подписчика"""
-    from django.core.mail import send_mail
-    from studies.models import Course, Subscribe
-    from config import settings
-
     subs = Subscribe.objects.filter(course_id=course_id).select_related('user', 'course')
     if subs.exists():
         course_title = subs[0].course.title
@@ -15,7 +14,7 @@ def send_course_update_email(course_id):
         send_mail(
             subject="Обновления курса",
             message=f"Появились обновления в рамках курса {course_title}",
-            from_email=settings.EMAIL_HOST_USER,
+            from_email=settings.EMAIL_HOST_USER,  # Используем из settings
             recipient_list=recipient_list,
             fail_silently=False,
         )
